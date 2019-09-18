bdf24124d77ec0fef25d161f218c90369748bb86
Your Name <you@example.com>
2019-09-17 21:14:31 -0700

    woring on seeting page

--- src/views/Dashboard/Settings.js
+++ src/views/Dashboard/Settings.js
@@ -1,29 +1,74 @@
 import React from 'react';
-import { Grid } from 'react-bootstrap'
+import { Grid, Table } from 'react-bootstrap'
 import { GetRequest } from '../../utils/ApiMethods'
+import { connect } from 'react-redux'
 
-class Settings extends React.Component{
-constructor(props){
-    super(props);
-    this.getUserProfile()
-}
+import { DateFormat} from '../../utils/helpers'
+import Button from "components/CustomButton/CustomButton";
 
-getUserProfile=()=>{
-GetRequest.getUser().then(res=>{
+class LinkRedirectOption extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: {}
+        }
+        this.getUserProfile()
+    }
 
-}).catch(err=>{
-    
-})
-}
+    getUserProfile = () => {
+        GetRequest.getUser().then(res => {
+            this.setState({ user: res.data.user })
+        }).catch(err => {
+         this.props.setNotification("error",err.message)
+        })
+    }
 
-    render(){
+    render() {
+        const { firstName, createdAt, email, lastName, totalAmountSpent, totalURLS, wallet } = this.state.user
+        return (
+            <Grid className="Setting_wrapper"fluid>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Full Name</th>
+                            <th>Email</th>
+                            <th>Mobile Number</th>
+                            <th>Member Since</th>
+                            <th>Balance Availed</th>
+                            <th>Balance Available</th>
+                            <th>Total Links</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{firstName + " " + lastName}</td>
+                            <td>{email}</td>
+                            <td>--</td>
+                            <td>{DateFormat(createdAt)}</td>
+                            <td>{totalAmountSpent}</td>
+                            <td>{wallet}</td>
+                            <td>{totalURLS}</td>
+                        </tr>
 
-        return(
-    <Grid fluid>
-        settings
-    </Grid>
+                    </tbody>
+                </Table>
+                <Button
+                    fill
+                    round
+                    onClick={this.onUnlock}
+                    className="btn"
+                    >
+                    {<i className="fa fa-pencil"/>}Edit
+                   </Button>
+            </Grid>
         )
     }
 }
 
-export default Settings;
+const mapDispatchToProps=dispatch=>{
+    return{
+      setNotification:(errorType,message)=>dispatch({type:'SET_NOTIFICATION',errorType:errorType,errorMessage:message}),
+    }
+  }
+  
+  export default connect(null,mapDispatchToProps)(LinkRedirectOption)
